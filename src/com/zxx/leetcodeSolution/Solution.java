9fd4d4e479ed2e1bfd9ec9389952168cab248727
xingxing.zhong <xingxing.zhong@helin-corp.com>
2014-11-03 13:17:12 +0800

    init my project

---
+++ src/com/zxx/leetcodeSolution/Solution.java
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+package com.zxx.leetcodeSolution;
+
+/**
+*----------Dragon be here!----------/
+* 　　　┏┓　　　┏┓
+* 　　┏┛┻━━━┛┻┓
+* 　　┃　　　　　　　┃
+* 　　┃　　　━　　　┃
+* 　　┃　┳┛　┗┳　┃
+* 　　┃　　　　　　　┃
+* 　　┃　　　┻　　　┃
+* 　　┃　　　　　　　┃
+* 　　┗━┓　　　┏━┛
+* 　　　　┃　　　┃神兽保佑
+* 　　　　┃　　　┃代码无BUG！
+* 　　　　┃　　　┗━━━┓
+* 　　　　┃　　　　　　　┣┓
+* 　　　　┃　　　　　　　┏┛
+* 　　　　┗┓┓┏━┳┓┏┛
+* 　　　　　┃┫┫　┃┫┫
+* 　　　　　┗┻┛　┗┻┛
+* ━━━━━━神兽出没━━━━━━
+* @date 2014年11月3日
+*/
+public class Solution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
